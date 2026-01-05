15166a89ce42a93e45f66ab34bb1553a0af6d1f3
shubhamNayak1 <itssnayak@gmail.com>
2026-01-05 17:26:48 +0530

    yes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/elogbookFrontEnd/",
   plugins: [react()],
   define: {
     // Ensuring process.env is defined for the browser-side Gemini SDK
